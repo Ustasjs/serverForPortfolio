1483cda592e91d5d5ee1c90b4a4780d34df45f3a
Ustiuzhanin Stanislav <ov.s.ust@gmail.com>
2018-01-15 10:44:07 +0300

    fix logger

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // cors убрать на продакшене
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://http://92.53.104.80:3000");
+  res.header("Access-Control-Allow-Origin", "http://92.53.104.80:3000");
   res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
--- logger.js
+++ logger.js
@@ -1,9 +1,22 @@
 const winston = require('winston');
+const logDir = 'logs';
+const fs = require('fs');
+
+if (!fs.existsSync(logDir)) {
+  fs.mkdirSync(logDir);
+}
 
 const logger = new (winston.Logger)({
   transports: [
-    new (winston.transports.File)({ filename: `${__dirname}/logs/combined.log`, level: 'info' })
-  ]
+    new (winston.transports.File)({
+      filename: `${logDir}/combined.log`,
+      level: 'info'
+    })
+  ],
+  exceptionHandlers: [
+    new winston.transports.File({ filename: `${logDir}/exceptions.log` })
+  ],
+  exitOnError: false
 });
 
 module.exports = logger;
